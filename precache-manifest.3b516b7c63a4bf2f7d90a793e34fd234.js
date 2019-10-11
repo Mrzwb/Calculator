c760e26f096bd44bc2b332362b2363c76e6ed70c
zhouwb15 <zhouwb15@chinauniocm.cn>
2019-10-11 16:59:33 +0800

    Updates

---
+++ precache-manifest.3b516b7c63a4bf2f7d90a793e34fd234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dda83a845704e147652d20b2ca0f5a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a1776880f8edf1b0e1f",
+    "url": "./static/css/main.4729480d.chunk.css"
+  },
+  {
+    "revision": "0c326155f03c618628c9",
+    "url": "./static/js/2.4b46a9ce.chunk.js"
+  },
+  {
+    "revision": "0a1776880f8edf1b0e1f",
+    "url": "./static/js/main.8916ffab.chunk.js"
+  },
+  {
+    "revision": "3e876a3051533500d705",
+    "url": "./static/js/runtime-main.2c5496a0.js"
+  }
+]);
